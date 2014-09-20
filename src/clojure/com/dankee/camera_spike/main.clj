7414d57832c53e0b9ae2cd558c37acff3914232d
Dan Kee <dan@dankee.com>
2014-09-19 21:48:31 -0500

    lein droid new camera-spike com.dankee.camera_spike :target-sdk 19 :app-name "Camera Spike"

---
+++ src/clojure/com/dankee/camera_spike/main.clj
@@ -0,0 +1,13 @@
+(ns com.dankee.camera_spike.main
+  (:use [neko.activity :only [defactivity set-content-view!]]
+        [neko.threading :only [on-ui]]
+        [neko.ui :only [make-ui]]))
+
+(defactivity com.dankee.camera_spike.MainActivity
+  :def a
+  :on-create
+  (fn [this bundle]
+    (on-ui
+     (set-content-view! a
+      (make-ui [:linear-layout {}
+                [:text-view {:text "Hello from Clojure!"}]])))))
