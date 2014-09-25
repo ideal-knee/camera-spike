15d9fcbead1ff5f577274676278d41bc332fcb63
Dan Kee <dan@dankee.com>
2014-09-25 10:15:14 -0500

    Clean up saving-media ns.

--- src/clojure/com/dankee/camera_spike/saving_media.clj
+++ src/clojure/com/dankee/camera_spike/saving_media.clj
@@ -1,32 +1,42 @@
-(ns com.dankee.camera_spike.saving-media)
+(ns com.dankee.camera_spike.saving-media
+  (:import [android.net Uri]
+           [android.os Environment]
+           [android.util Log]
+           [java.io File]
+           [java.text SimpleDateFormat]
+           [java.util Date] ) )
 
 ; Port of https://developer.android.com/guide/topics/media/camera.html#saving-media
 
 (def media-type-image 1)
 (def media-type-video 2)
 
-(defn create-media-file [type media-storage-dir]
-  (let [timestamp (.. (new java.text.SimpleDateFormat "yyyyMMdd_HHmmss")
-                      (format (new java.util.Date)) )]
+(defn- create-media-file [type media-storage-dir]
+  (let [timestamp (.format (new SimpleDateFormat "yyyyMMdd_HHmmss") (new Date))]
     (case type
-      media-type-image (new java.io.File (str (.getPath media-storage-dir)
-                                              java.io.File/separator
+      media-type-image (new File (str (.getPath media-storage-dir)
+                                              File/separator
                                               "IMG_" timestamp ".jpg"))
-      media-type-video (new java.io.File (str (.getPath media-storage-dir)
-                                              java.io.File/separator
+      media-type-video (new File (str (.getPath media-storage-dir)
+                                              File/separator
                                               "VID_" timestamp ".mp4"))
       nil ) ) )
 
-(defn get-output-media-file [type]
-  (let [media-storage-dir (new java.io.File
-                               (android.os.Environment/getExternalStoragePublicDirectory
-                                android.os.Environment/DIRECTORY_PICTURES )
+(defn- get-output-media-file [type]
+  (let [media-storage-dir (new File
+                               (Environment/getExternalStoragePublicDirectory Environment/DIRECTORY_PICTURES)
                                "Camera Spike" )]
     (if (and (not (.exists media-storage-dir))
              (not (.mkdirs media-storage-dir)) )
-      (android.util.Log/d "Camera Spike" "failed to create directory")
+      (Log/d "Camera Spike" "failed to create directory")
       (create-media-file type media-storage-dir) ) ) )
 
-(defn get-output-media-file-uri [type]
-  (android.net.Uri/fromFile (get-output-media-file type)) )
+(defn- get-output-media-file-uri [type]
+  (Uri/fromFile (get-output-media-file type)) )
+
+(defn get-output-image-uri []
+  (get-output-media-file-uri media-type-image) )
+
+(defn get-output-video-uri []
+  (get-output-media-file-uri media-type-video) )
 
